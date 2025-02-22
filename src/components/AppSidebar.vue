200e6922b0f0adb828fb14a37c3ebba71746f11a
erickhilda <erickh.andreas@gmail.com>
2025-02-22 18:42:20 +0700

    chore: setup shadcn

---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import {
+  IconCalendar,
+  IconChevronDown,
+  IconHome,
+  IconInbox,
+  IconPlus,
+  IconSearch,
+  IconSettings,
+} from "@tabler/icons-vue";
+import { DropdownMenuRoot } from "reka-ui";
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: IconHome,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: IconInbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: IconCalendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: IconSearch,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: IconSettings,
+  },
+];
+</script>
+
+<template>
+  <Sidebar>
+    <SidebarHeader>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <DropdownMenuRoot>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton>
+                Select Workspace
+                <IconChevronDown class="ml-auto" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent class="w-[--bits-dropdown-menu-anchor-width]">
+              <DropdownMenuItem>
+                <span>Acme Inc</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Acme Corp.</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenuRoot>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarHeader>
+
+    <SidebarContent>
+      <SidebarGroup>
+        <SidebarGroupLabel>Projects</SidebarGroupLabel>
+        <SidebarGroupAction title="Add Project">
+          <IconPlus /> <span class="sr-only">Add Project</span>
+        </SidebarGroupAction>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            <SidebarMenuItem v-for="item in items" :key="item.title">
+              <SidebarMenuButton asChild>
+                <a :href="item.url">
+                  <component :is="item.icon" />
+                  <span>{{ item.title }}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </SidebarContent>
+  </Sidebar>
+</template>
